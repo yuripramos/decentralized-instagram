08111122ddb75747d1279b910a9440cad76d7e66
Yuri Ramos <yuri.ramos@anedot.com>
2021-07-07 16:43:49 -0300

    Submit Images to IPFS

--- src/components/App.js
+++ src/components/App.js
@@ -6,12 +6,31 @@ import Decentragram from '../abis/Decentragram.json'
 import Navbar from './Navbar'
 import Main from './Main'
 
+const ipfsClient = require('ipfs-http-client')
+const ipfs = ipfsClient({host: 'ipfs.infura.io', port: 5001, protocol: 'https'})
+
 
 const App = () => {
 
   const [account, setAccount] = useState('');
-  const [loading, setLoading] = useState(false)
+  const [buffer, setBuffer] = useState('')
+  const [decentragram, setDecentragram] = useState(null)
+  const [images, setImages] = useState([])
+  const [loading, setLoading] = useState(true)
 
+
+  const captureFile = event => {
+
+    event.preventDefault()
+    const file = event.target.files[0]
+    const reader = new window.FileReader()
+    reader.readAsArrayBuffer(file)
+
+    reader.onloadend = () => {
+      setBuffer(Buffer(reader.result))
+      console.log('Buffer => ', buffer)
+    }
+  }
 
   const loadWeb3 = async () => {
     if(window.ethereum) {
@@ -31,11 +50,41 @@ const App = () => {
 
     const accounts = await web3.eth.getAccounts()
     setAccount(accounts[0])
+
+    const networkId = await web3.eth.net.getId()
+    const networkData = Decentragram.networks[networkId]
+    if(networkData) {
+      const decentragram = web3.eth.Contract(Decentragram.abi, networkData.address)
+      setDecentragram(decentragram)
+      const imagesCount = await decentragram.methods.imageCount().call()
+      setImages(imagesCount)
+      setLoading(false)
+    } else {
+      window.alert('Decentragram contract not deployed to detect network.')
+    }
   }
 
-  useEffect(async () => {
-    await loadWeb3()
-    await loadBlockchainData()
+  const uploadImage = description => {
+    console.log("Submitting files to ipfs")
+
+    //Adding file to the IPFS
+    ipfs.add(buffer, (error, result) => {
+      console.log('Ipfs result', result)
+      if(error) {
+        console.log(error)
+        return
+      }
+
+      // setLoading(true)
+      // decentragram.methods.uploadImage(result[0].hash, description).send({ from: account}).on('transactionHash', hash => {
+      //   setLoading(false)
+      // })
+    })
+  }
+
+  useEffect(() => {
+    loadWeb3()
+    loadBlockchainData()
   }, [])
 
   return (
@@ -44,7 +93,9 @@ const App = () => {
       {loading
         ? <div id="loader" className="text-center mt-5"><p>Loading...</p></div>
         : <Main
-          // Code...
+          captureFile={captureFile}
+          uploadImage={uploadImage}
+          images={images}
           />
         }
     </div>
