8882942dea44077b4e15a7207788c58d6120a6c4
Yuri Ramos <yuri.ramos@anedot.com>
2021-07-07 12:51:58 -0300

    Connect localwallet with Metamask
    development wallet

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Web3 from 'web3';
 import Identicon from 'identicon.js';
 import './App.css';
@@ -11,6 +11,32 @@ const App = () => {
 
   const [account, setAccount] = useState('');
   const [loading, setLoading] = useState(false)
+
+
+  const loadWeb3 = async () => {
+    if(window.ethereum) {
+      window.web3 = new Web3(window.ethereum)
+      await window.ethereum.enable()
+    }
+    else if (window.web3) {
+      window.web3 = new Web3(window.web3.currentProvider)
+    }
+    else {
+      window.alert('Non-Ethereum browser Detected. You should consider trying MetaMask!')
+    }
+  }
+
+  const loadBlockchainData = async () => {
+    const web3 = window.web3
+
+    const accounts = await web3.eth.getAccounts()
+    setAccount(accounts[0])
+  }
+
+  useEffect(async () => {
+    await loadWeb3()
+    await loadBlockchainData()
+  }, [])
 
   return (
     <div>
