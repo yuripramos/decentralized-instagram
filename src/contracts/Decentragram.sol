55fbb1ececbbdcf7f0cbd04aa66b721806e02878
Yuri Ramos <yuri.ramos@anedot.com>
2021-07-07 12:02:51 -0300

    Update Decentragram Contract
    Adds `uploadImage` and `tipImageOwned` within the smart contract

--- src/contracts/Decentragram.sol
+++ src/contracts/Decentragram.sol
@@ -2,4 +2,79 @@ pragma solidity ^0.5.0;
 
 contract Decentragram {
     string public name = "Decentragram";
+
+    // Store Image
+    uint256 public imageCount = 0;
+    mapping(uint256 => Image) public images;
+    struct Image {
+        uint256 id;
+        string hash;
+        string description;
+        uint256 tipAmount;
+        address payable author;
+    }
+
+    event ImageCreated(
+        uint256 id,
+        string hash,
+        string description,
+        uint256 tipAmount,
+        address payable author
+    );
+
+    event ImageTipped(
+        uint256 id,
+        string hash,
+        string description,
+        uint256 tipAmount,
+        address payable author
+    );
+
+    // Create Images
+    function uploadImage(string memory _imgHash, string memory _description)
+        public
+    {
+        require(bytes(_imgHash).length > 0);
+
+        require(bytes(_description).length > 0);
+
+        require(msg.sender != address(0x0));
+
+        imageCount++;
+
+        images[imageCount] = Image(
+            imageCount,
+            _imgHash,
+            _description,
+            0,
+            msg.sender
+        );
+
+        //Trigger an event
+        emit ImageCreated(imageCount, _imgHash, _description, 0, msg.sender);
+    }
+
+    // Tip Images
+
+    function tipImageOwner(uint256 _id) public payable {
+        require(_id > 0 && _id <= imageCount);
+        //fetch
+        Image memory _image = images[_id];
+        //fetch the author
+        address payable _author = _image.author;
+        //pay the author by sending them Ether
+        address(_author).transfer(msg.value);
+        //Increment the tip amount
+        _image.tipAmount = _image.tipAmount + msg.value;
+        //Update the image
+        images[_id] = _image;
+
+        emit ImageTipped(
+            _id,
+            _image.hash,
+            _image.description,
+            _image.tipAmount,
+            _author
+        );
+    }
 }
